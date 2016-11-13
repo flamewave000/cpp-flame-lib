472f89276788574b517e4a77a56f5cb36f198115
Andrew F <flamewave000@live.com>
2016-11-12 23:42:31 -0500

    object is now fully functional with protocols
    Un fortunately I could not figure out how to get a protocol to recast itself to
    an object * within its constructor. This means that every class that implements
    a protocol will also have to register that protocol manually. This is done using
    the shorthand IMPLEMENTS(PROTO([protocol1]), PROTO([protocol2], ...)

--- include/object.h
+++ include/object.h
@@ -5,23 +5,29 @@
 #include <memory>
 #include <vector>
 
-namespace flame {
+namespace std {
 	class object;
 
+	/*wraps the info and relations for a single runtime type*/
 	class type final {
 #pragma region internal classes
 	public:
+		/*contains the class info of a single runtime type*/
 		class info final {
 #pragma region internal classes
 		public:
+			/*unique identifier*/
 			typedef size_t uid;
 #pragma endregion
 
 
 #pragma region instance variables
 		private:
+			/*the unique id of the class in the form of a RTTI hash code*/
 			const uid _id;
+			/*the runtime class name*/
 			const char * _name;
+			/*true if the class is a protocol; otherwise false*/
 			const bool _is_protocol;
 #pragma endregion
 
@@ -34,8 +40,11 @@ namespace flame {
 
 #pragma region properties
 		public:
+			/*returns the unique id in the form of a RTTI hash code*/
 			inline const uid get_id() const { return _id; }
+			/*returns the runtime class name*/
 			inline const char * get_name() const { return _name; }
+			/*returns true if the class is a protocol; otherwise false*/
 			inline bool is_protocol() const { return _is_protocol; }
 #pragma endregion
 
@@ -63,15 +72,20 @@ namespace flame {
 
 #pragma region instance variables
 	private:
+		/*the std::type::info for the runtime host class this std::type represents*/
 		info _info;
+		/*the base std::type the host class inherits from; null if we are std::object*/
 		const std::shared_ptr<type> _base;
+		/*the list of protocols the host class implements*/
 		std::vector<info> _protocols;
 #pragma endregion
 
 
 #pragma region properties
 	public:
+		/*returns the std::type::info for the host class*/
 		inline const info &get_info() const { return _info; }
+		/*returns the std::stype of the base class the host class inherits from*/
 		inline const type &get_base_type() const { return *_base; }
 #pragma endregion
 
@@ -84,6 +98,8 @@ namespace flame {
 
 #pragma region public methods
 	public:
+		/*returns true if the provided std::type::info is a protocol type and is
+		registered as being implemented by the host class; otherwise false*/
 		bool has_protocol(const info &proto_info) const {
 			if (proto_info._is_protocol) {
 				for (size_t c = 0, size = _protocols.size(); c < size; c++) {
@@ -97,6 +113,9 @@ namespace flame {
 			}
 			return false;
 		}
+		/*returns true if the provided std::type::info is not a protocol and has the
+		same std::type::info::uid as the host class or any of its base classes;
+		otherwise false*/
 		inline bool has_class(const info &type_info) const {
 			return !type_info._is_protocol && (_info == type_info || (_base != nullptr && const_cast<type*>(_base.get())->has_class(type_info)));
 		}
@@ -118,44 +137,53 @@ namespace flame {
 	};
 
 
-
+	/*helper macro for properly creating an instance of std::type::info using RTTI*/
 #define TYPE_INFO(c, p) type::info(typeid(c).hash_code(), typeid(c).name(), p)
+	/*returns the runtime std::type::info for the templated class type*/
 	template<class _obj>
 	inline type::info class_info() { return TYPE_INFO(_obj, false); }
+	/*returns the runtime std::type::info for the templated protocol type*/
 	template<class _obj>
 	inline type::info proto_info() { return TYPE_INFO(_obj, true); }
 
-
+	/*registers the provided derived type as being the host class*/
 #define DERIVED(derived) this->__reg_type<derived>()
+	/*registers the list of protocol std::type::info as being implemented by the host class*/
 #define IMPLEMENTS(...) this->__reg_protos({__VA_ARGS__});
-#define PROTO(protocol) type::info(typeid(protocol).hash_code(), typeid(protocol).name(), true)
+	/*short hand helper for retrieving the std::type::info of a protocol*/
+#define PROTO(protocol) std::proto_info<protocol>()
+	/*short hand helper for extending std::object as the base*/
+#define ROOT_BASE public std::object
 
+	/*the root base class of all typed classes*/
 	class object {
 #pragma region instance variables
 	private:
+		/*the std::type of the host runtime class*/
 		std::shared_ptr<type> _type;
 #pragma endregion
 
 
 #pragma region constructors
 	public:
-		object() {
-			__reg_type<object>();
-		}
+		object() { __reg_type<object>(); }
 		virtual ~object() {}
 #pragma endregion
 
 
 #pragma region protected methods
 	protected:
+		/*registers a single type as the host runtime class*/
 		template<class _obj>
 		inline void __reg_type() {
 			_type.reset(new type(TYPE_INFO(_obj, false), _type));
 		}
+		/*registers a single protocol that is implemented by the host runtime class*/
 		template<class _obj>
 		inline void __reg_proto() {
 			_type->_protocols.push_back(TYPE_INFO(_obj, true));
 		}
+		/*registers a list of protocol std::type::info that are implemented by this runtime class*/
 		inline void __reg_protos(const std::vector<type::info> &infos) {
 			for (size_t c = 0, size = infos.size(); c < size; c++) {
 				_type->_protocols.push_back(infos[c]);
@@ -166,9 +194,12 @@ namespace flame {
 
 #pragma region public methods
 	public:
+		/*returns true if this class is an instance of the provided object*/
 		inline bool instanceof(const object *obj) const {
 			return instanceof(obj->get_type().get_info());
 		}
+		/*returns true if this class inherits from the class or proto described
+		provided by the std::type::info*/
 		inline bool instanceof(const type::info &obj_type) const {
 			return (obj_type._is_protocol && _type->has_protocol(obj_type)) || _type->has_class(obj_type);
 		}
@@ -178,24 +209,10 @@ namespace flame {
 	};
 
 
-	class protocol {
-#pragma region constructors
-	protected:
-		protocol() {
-		}
-	public:
-		virtual ~protocol() {}
-#pragma endregion
-
-
-#pragma region protected methods
-	protected:
-		inline const char * to_string() const { return dynamic_cast<const object*>(this)->to_string(); }
-#pragma endregion
-	};
+#define PROTOCOL(p_name) class p_name {protected:p_name(){}public
+#define PROTOEND };
 
 #undef TYPE_INFO
-
 }
 
 #endif
